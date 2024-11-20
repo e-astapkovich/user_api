a95eccae7bece8460b8951fc748be16fb226acc1
e-astap <ni-shagu-nazad@yandex.ru>
2024-11-20 13:34:32 +0500

    Добавил поле age

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,17 +26,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'age' => fake()->numberBetween(1, 100),
             'password' => static::$password ??= Hash::make('password'),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'age' => 25,
         ]);
+
+        User::factory(10)->create();
     }
 }
